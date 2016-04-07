7ee928fd83694b5936b58db1b2572bfe2cbca20c
Mina <wilhelmina.jane@gmail.com>
2016-04-07 16:50:57 +0100

    conflict resolved

--- src/js/components/app-container.jsx
+++ src/js/components/app-container.jsx
@@ -5,7 +5,7 @@ class AppContainer extends React.Component {
     render () {
 
         return (
-            <div
+            <div>
                 <h1>This is the app container</h1>
             </div>
         );
