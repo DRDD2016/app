797fa66debeed4caf66084667b90cc78fc67a344
Mina <wilhelmina.jane@gmail.com>
2016-04-07 15:28:44 +0100

    [WIP] initial setup (#1)

---
+++ src/utils/dev-server.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('../../webpack.development.config.js');
+
+const compiler = webpack(config);
+
+const options = {
+    contentBase: 'public/',
+    hot: true,
+    filename: 'bundle.js',
+    stats: {
+        colors: true
+    }
+};
+
+const server = new WebpackDevServer(compiler, options);
+
+server.listen(8080, 'localhost', function () {
+    console.log('webpack-dev-server is running on 8080');
+});
---
+++ test/fake-test.jsx
@@ -0,0 +1,9 @@
+'use strict';
+
+import test from 'tape';
+
+test('a test', (t) => {
+
+    t.equal(1, 1, "one equals one");
+    t.end();
+});
