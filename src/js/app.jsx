b4aea090aa21e8d8e632fc4c0d5da3813fa95e29
sohilpandya <sohilpandya@me.com>
2016-04-12 15:00:55 +0100

    setting up react router routes

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -7,12 +7,27 @@ require('../scss/main.scss');
 import AppContainer from './components/app-container.jsx';
 import Login from './components/login.jsx';
 import Feed from './components/feed.jsx';
+import CreateEventContainer from './components/create-event-container.jsx';
+import EventDetails from './components/event-details.jsx';
+import EventWhat from './components/event-what.jsx';
+import EventWhen from './components/event-when.jsx';
+import EventWhere from './components/event-where.jsx';
+
 
 
 const routes = (
     <Route path='/' component={ AppContainer }>
         <IndexRoute component={Login} />
         <Route path='/feed' component={Feed} />
+
+    <Route path='/create-event' component={CreateEventContainer} >
+        <IndexRoute component={EventDetails} />
+        <Route path='what' component={EventWhat} />
+        <Route path='where' component={EventWhere} />
+        <Route path='when' component={EventWhen} />
+    </Route>
+
+
     </Route>
 )
 
