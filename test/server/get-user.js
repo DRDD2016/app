6b29b516c1273addb6d5f2b63153990ef9e1e9d4
Mina <wilhelmina.jane@gmail.com>
2016-04-23 20:11:58 +0100

    tidy up

--- test/server/get-user.js
+++ test/server/get-user.js
@@ -1,9 +1,8 @@
 import test from 'tape';
 var testServer = require('../../server/index.js');
 var server = testServer.init(9001);
-// console.log(":::::::",testServer);
 
-test.only('`get-user` request fetches user information from database', (t) => {
+test('`get-user` request fetches user information from database', (t) => {
 
     const options = {
         method: 'GET',
@@ -11,9 +10,8 @@ test.only('`get-user` request fetches user information from database', (t) => {
     };
 
     server.inject(options, (response) => {
-        console.log(response.result);
+
         t.equal(response.result.id, "10153623802657825", "User information retrieved");
     });
-    server.stop(function(){});
-    t.end();
+    server.stop(t.end);
 });
