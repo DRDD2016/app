647c2f7b1eb73da0645a822286c380d1a032c2c5
Mina <wilhelmina.jane@gmail.com>
2016-04-23 16:23:41 +0100

    db init tidy up.  now accepts a production config var

--- server/db/init.js
+++ server/db/init.js
@@ -1,26 +1,23 @@
 var url = require('url');
 var redis = require('redis');
 var bluebird = require('bluebird');
-var dbURL = process.env.REDISCLOUD_URL;
 
-// bluebird.promisifyAll(redis.redisClient.prototype);
-// bluebird.promisifyAll(redis.multi.prototype);
+bluebird.promisifyAll(redis);
 
-bluebird.promisifyAll(require("redis"));
-
+var dbURL = process.env.PRODUCTION ? "redis://localhost:6379" : process.env.REDISCLOUD_URL;
 
 if (!dbURL) {
 
     throw new Error("Redis environment variable has not been set");
 }
 
-var redisURL = url.parse(dbURL);
+var parsedURL = url.parse(dbURL);
+var client = redis.createClient(parsedURL.port, parsedURL.hostname, { no_ready_check: true });
 
-var client = redis.createClient(redisURL.port, redisURL.hostname, { no_ready_check: true });
-client.auth(redisURL.auth.split(':')[1]);
+client.auth(parsedURL.auth.split(':')[1]);
 
 client.on('error', (err) => {
-    console.log('Redis error: ' + err);
+    throw new Error(err);
 });
 
 module.exports = client;
