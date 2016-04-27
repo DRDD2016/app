6f3d2917d73fe37113afb842599762c2433f9973
sohilpandya <sohilpandya@me.com>
2016-04-27 15:20:16 +0100

    adds development env for database should fix issue where user is unable to log in

---
+++ server/db/init.js
@@ -0,0 +1,23 @@
+var url = require('url');
+var redis = require('redis');
+var bluebird = require('bluebird');
+
+bluebird.promisifyAll(redis);
+
+// var dbURL = process.env.REDIS_URL;
+var dbURL = process.env.DEVELOPMENT || process.env.REDIS_URL;
+
+
+var parsedURL = url.parse(dbURL);
+var client = redis.createClient(parsedURL.port, parsedURL.hostname, { no_ready_check: true });
+
+if (parsedURL.auth) {
+
+    client.auth(parsedURL.auth.split(':')[1]);
+}
+
+client.on('error', (err) => {
+    throw new Error(err);
+});
+
+module.exports = client;
