f43ba89b88eef64d415b03535b2471f8d91bbd18
sohilpandya <sohilpandya@me.com>
2016-04-28 19:25:36 +0100

    adds test for adding multiple events

--- test/server/db/test.js
+++ test/server/db/test.js
@@ -2,51 +2,59 @@ import test from 'tape';
 import client from '../../../server/db/init.js';
 import saveNewEvent from '../../../server/db/saveNewEvent.js';
 
+const eventWhat = JSON.stringify({
+    0: "Bowling",
+    1: "Swimming"
+});
+const eventWhere = JSON.stringify({
+    0: {
+        placeName: "Harrods",
+        placeAddress: "Knightsbridge, London"
+    }
+});
+const eventWhen = JSON.stringify({
+    0: {
+        date: "2016-11-10",
+        time: "12:00"
+    }
+});
+const invitees = JSON.stringify([12345678]);
+const hostID = 10154129575200996;
+const isPoll = true;
+
+const event = {
+    eventWhat,
+    eventWhere,
+    eventWhen,
+    invitees,
+    hostID,
+    isPoll
+};
+
 test('New event is set correctly', (t) => {
-
-    const eventWhat = JSON.stringify({
-        0: "Bowling",
-        1: "Swimming"
-    });
-    const eventWhere = JSON.stringify({
-        0: {
-            placeName: "Harrods",
-            placeAddress: "Knightsbridge, London"
-        }
-    });
-    const eventWhen = JSON.stringify({
-        0: {
-            date: "2016-11-10",
-            time: "12:00"
-        }
-    });
-    const invitees = JSON.stringify([12345678]);
-    const hostID = 10154129575200996;
-    const isPoll = true;
-
-    const event = {
-        eventWhat,
-        eventWhere,
-        eventWhen,
-        invitees,
-        hostID,
-        isPoll
-    };
 
     saveNewEvent(event, (error, result) => {
         t.notOk(error, "New event saves without error");
         t.end();
     });
-    const actual = client.hmset("event:1", "eventName", "Tormod's birthday bash",
-                                "eventDescription", "Let's have a ball!",
-                                "eventWhat", eventWhat, "eventWhere", eventWhere, "eventWhen", eventWhen,
-                                "invitees", invitees, "hostID", hostID, "isPoll", isPoll,
-                                (error, reply) => {
-                                    console.log(error, reply);
-                                });
+});
+
+test('saveNewEvent handles adding subsequent events', (t) => {
+
+    saveNewEvent(event, (error, result) => {
+
+        client.exists("event:2", (error, result) => {
+            t.ok(result);
+            t.end();
+        });
+    });
 });
 
 test('TEST TEARDOWN', (t) => {
+
+    client.del("eventKeys");
+    client.del("event:1");
+    client.del("event:2");
 
     t.end();
     client.quit();
