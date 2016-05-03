d72bbb3974fd52a9bedc52b57a5c3fbb79dfe052
Mina <wilhelmina.jane@gmail.com>
2016-05-03 17:45:05 +0100

    added harry potter to the fake db for testing

--- test/utils/initDB.js
+++ test/utils/initDB.js
@@ -6,3 +6,10 @@ client.hmsetAsync("user:10154129575200996",
                  'id', 10154129575200996,
                  'token', "notokenhere",
                  'photoURL', "https://scontent.xx.fbcdn.net/v/t1.0-1/p320x320/12821522_10154022257850996_5243104697257562021_n.jpg?oh=3b82501dc03ce1aa1d0db64278f606fa&oe=57AC45AB");
+
+client.hmsetAsync("user:12345678",
+          'firstName', "Harry",
+          'lastName', "Potter",
+          'id', 12345678,
+          'token', "harryToken",
+          'photoURL', "http://harrypotter.com/photo.jpg");
