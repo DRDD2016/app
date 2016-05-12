7bcf05b8e487edc2edd29a4b46d2a70b4564b80f
Mina <wilhelmina.jane@gmail.com>
2016-05-12 11:50:47 +0100

    endpoint test for getUser #26

--- test/server/endpoints.js
+++ test/server/endpoints.js
@@ -1,30 +1,30 @@
-// import test from 'tape';
-// var testServer = require('../../server/index.js');
-//
-// test.skip('`getUser` retrieves user information from database EXCLUDING token', (t) => {
-//
-//     if (!process.env.REDISCLOUD_URL) {
-//         throw new Error("Please set the test db url");
-//     }
-//
-//     const options = {
-//         method: 'GET',
-//         url: '/get-user?sparkID=12345678'
-//     };
-//
-//     const expected = {
-//         firstName: "Harry",
-//         lastName: "Potter",
-//         id: "87654321",
-//         photoURL: "http://blog.mugglenet.com/wp-content/uploads/2014/03/harry-potter-and-the-goblet-of-fire-20050330083044751-e1394684064435.jpg"
-//     };
-//
-//     testServer.init(9001, (error, server) => {
-//
-//         server.inject(options, (response) => {
-//
-//             t.deepEqual(response.result, expected, "Correct user information retrieved");
-//         });
-//         server.stop(t.end);
-//     });
-// });
+import test from 'tape';
+var testServer = require('../../server/index.js');
+
+test('`getUser` retrieves user information from database EXCLUDING token', (t) => {
+
+    if (!process.env.DEVELOPMENT) {
+        throw new Error("Please set the test db url");
+    }
+
+    const options = {
+        method: 'GET',
+        url: '/get-user?userID=12345678'
+    };
+
+    const expected = {
+        firstName: "Harry",
+        lastName: "Potter",
+        id: "12345678",
+        photoURL: "http://harrypotter.com/photo.jpg"
+    };
+
+    testServer.init(9001, (error, server) => {
+
+        server.inject(options, (response) => {
+
+            t.deepEqual(response.result, expected, "Correct user information retrieved");
+        });
+        server.stop(t.end);
+    });
+});
