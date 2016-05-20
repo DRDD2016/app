98691ed8f3bbcb8bbd2acb6e84af1a12d108283a
Mina <wilhelmina.jane@gmail.com>
2016-05-20 18:00:32 +0100

    adds moment locale, adds font awesome pseudo elements. creates calendar view #143 #144. creates function for uniform date formatting

--- src/js/components/notification.jsx
+++ src/js/components/notification.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
+import formatDate from '../lib/formatDate.js';
 
 const Notification = ({ eventID, timestamp, firstName, lastName, photoURL, eventWhat, eventWhere, eventWhen, userIsHost, isPoll, subjectID, hostID }) => {
 
     let hostIsSubject = subjectID === hostID;
-    
+
     return (
         <Link to={ 'event/' + eventID } >
-            <div className="row notifications">
+            <div className="row notification">
                 <img className="three columns ui profile-photo circular image" src={ photoURL } alt="Host photo" />
                 <div className="six columns">
                     <p className="timestamp"> { moment(timestamp).startOf().fromNow() } </p>
-                    <p className="name">
+                    <p className="subject-name">
                         { hostIsSubject && " You"}
                         { !hostIsSubject && firstName + " " + lastName }
                     <span>
@@ -25,16 +26,19 @@ const Notification = ({ eventID, timestamp, firstName, lastName, photoURL, event
                     </p>
                 </div>
                 <div className="three columns event-type">
-                    <label>
-                        { eventWhat.length === 1 ? eventWhat[0] : "TBC" }
+                    <label className="eventWhere-placeName">
+                        { eventWhere.length === 1 ? eventWhere[0].placeName : "Vote" }
                     </label>
-                    <label>
-                        { eventWhere.length === 1 ? eventWhere[0].placeName : "TBC" }
+                    <label className="eventWhen-date">
+                        { eventWhen.length === 1 ? formatDate(eventWhen[0].date) : "Vote" }
                     </label>
-                    <label>
-                        { eventWhen.length === 1 ? eventWhen[0].date : "TBC" }
+                    <label className="eventWhen-time">
+                        { eventWhen.length === 1 ? eventWhen[0].time : "Vote" }
                     </label>
                 </div>
+            </div>
+            <div className="row">
+                <hr className="twelve columns" />
             </div>
         </Link>
     );
---
+++ src/js/lib/formatDate.js
@@ -0,0 +1,4 @@
+export default function formatDate (date) {
+
+    return moment(date).format('LL');
+}
