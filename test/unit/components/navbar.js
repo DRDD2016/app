87f22bfe26ed18e3a8aa4af5033ff0b2d44ddddc
Mina <wilhelmina.jane@gmail.com>
2016-05-27 12:28:58 +0100

    simple test for navbar.  mounting test is WIP #26

--- test/unit/components/navbar.js
+++ test/unit/components/navbar.js
@@ -1,2 +1,22 @@
 import test from 'tape';
-import TestUtils from 'react-addons-test-utils';
+import React from 'react';
+import { renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate } from 'react-addons-test-utils';
+import shallowRenderer from '../../utils/shallowRenderer.js';
+import Navbar from '../../../src/js/components/general/navbar.jsx';
+
+
+test('Navbar shallow renders correctly', (t) => {
+
+    const shallow = shallowRenderer(<Navbar />);
+    t.equal(shallow.props.children.length, 5, 'Navbar has 5 children');
+    t.end();
+});
+
+
+test.skip('Navbar renders onto DOM correctly', (t) => {
+
+    const rendered = renderIntoDocument(<Navbar />);
+    const buttons = scryRenderedDOMComponentsWithClass('nav-button');
+    // console.log(buttons);
+    t.end();
+});
