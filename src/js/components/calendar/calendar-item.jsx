cf20426b1a90412447ecd881f79a61d0e6c980c4
Mina <wilhelmina.jane@gmail.com>
2016-06-01 18:00:52 +0100

    improvement to calendar layout #143

--- src/js/components/calendar/calendar-item.jsx
+++ src/js/components/calendar/calendar-item.jsx
@@ -10,13 +10,13 @@ const CalendarItem = ({ eventName, eventWhat, eventWhere, eventWhen, eventID, ho
                 <div className="row">
                     <img className="three columns ui profile-photo circular image" src={ hostPhotoURL } alt="Host photo" />
 
-                    <div className="six columns">
+                    <div className="five columns">
                         <h5>{ eventName }</h5>
-                        <h6 className="eventWhen-date">{ formatDate(eventWhen[0].date) }</h6>
+                        <h6 className="eventWhen-date">{ ` ${formatDate(eventWhen[0].date)}` }</h6>
                     </div>
-                    
-                    <div className="three columns">
-                        <label className="eventWhere-placeName">{ `${eventWhere[0].placeName} ${eventWhere[0].placeAddress}` }</label>
+
+                    <div className="four columns">
+                        <label className="eventWhere-placeName">{ ` ${eventWhere[0].placeName} ${eventWhere[0].placeAddress}` }</label>
                     </div>
                 </div>
             </Link>
