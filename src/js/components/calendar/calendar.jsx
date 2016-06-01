71c6ba59f1335f4ec6acb721d9cb7b294d01b0ea
Mina <wilhelmina.jane@gmail.com>
2016-06-01 16:54:50 +0100

    [WIP #143]

--- src/js/components/calendar/calendar.jsx
+++ src/js/components/calendar/calendar.jsx
@@ -5,13 +5,20 @@ const Calendar = ({ data, isFetching, fetchEvent }) => {
 
     return (
         <div className="container">
+            <div className="row">
+                <h4 className="twelve columns">
+                    Calendar
+                </h4>
+            </div>
             {
                 data.map((item, i) => {
+                    
                     return <CalendarItem key={ i }
                                          eventName={ item.eventName }
                                          eventWhat={ item.eventWhat }
                                          eventWhere={ item.eventWhere }
                                          eventWhen={ item.eventWhen }
+                                         hostPhotoURL={ item.hostPhotoURL }
                                          eventID={ item.eventID } />;
                 })
             }
