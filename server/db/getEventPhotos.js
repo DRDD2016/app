eca0de2d1b24524703ec7f138a4107f835625dca
sohilpandya <sohilpandya@me.com>
2016-06-02 15:51:06 +0100

    get event photos [WIP]

--- server/db/getEventPhotos.js
+++ server/db/getEventPhotos.js
@@ -3,6 +3,14 @@ var client = require('./init.js');
 function getEventPhotos (eventID, callback) {
     var listName = 'photos:' + eventID;
     client.lrangeAsync(listName, 0, -1)
-        .then()
+        .then((response) => {
+            
+            callback(null, response);
+        })
+        .catch((error) => {
+            callback(error);
+        });
         //WIP
- }
+}
+
+module.exports = getEventPhotos;
