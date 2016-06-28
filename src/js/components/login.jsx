d4c3de2ac8e3fcd2800429eb582d770ecfe8e351
Mina <wilhelmina.jane@gmail.com>
2016-06-28 09:55:06 +0100

    removes banner image from login page #346

--- src/js/components/login.jsx
+++ src/js/components/login.jsx
@@ -24,29 +24,30 @@ class Login extends React.Component {
         return (
             <div>
 
-                    <div className="row login-event-header">
-                    </div>
-                    <div className="container login">
+                <div className="container login">
+                    <div className="row">
+
                         <img className="twelve columns" src="https://cloud.githubusercontent.com/assets/2305591/16362624/f05404f6-3bab-11e6-8b3a-0844c9b98589.png"/>
-
-                        <div className="row">
-                            <h4 className="text-snippet">
-                                The easy way to organise parties, events, share pictures and memories with friends, family and groups.
-                            </h4>
-                        </div>
-
-                        <div className="row">
-                            <button className="twelve columns facebook" onClick={ (e) => this.props.login() }>
-                                Login with Facebook
-                            </button>
-                        </div>
-
-                        <div className="row save-to-homescreen">
-                            <h5>
-                                Save the web-app to homescreen for the best experience
-                            </h5>
-                        </div>
                     </div>
+
+                    <div className="row">
+                        <h4 className="text-snippet">
+                            The easy way to organise parties, events, share pictures and memories with friends, family and groups.
+                        </h4>
+                    </div>
+
+                    <div className="row">
+                        <button className="twelve columns facebook" onClick={ (e) => this.props.login() }>
+                            Login with Facebook
+                        </button>
+                    </div>
+
+                    <div className="row save-to-homescreen">
+                        <h5>
+                            Save the web-app to homescreen for the best experience
+                        </h5>
+                    </div>
+                </div>
 
             </div>
         );
