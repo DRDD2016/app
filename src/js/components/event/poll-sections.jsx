e70503eee9629afe72b2ca0c4b5b4544fd68e498
Mina <wilhelmina.jane@gmail.com>
2016-06-29 17:15:17 +0100

    removes invisible icon, replaces with before pseudoclass

--- src/js/components/event/poll-sections.jsx
+++ src/js/components/event/poll-sections.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import formatDate from '../../lib/formatDate.js';
+import classnames from 'classnames';
 
 export const EventWhatSection = ({ text, tally, choiceClasses, labelClasses }) => {
     return (
@@ -15,16 +16,26 @@ export const EventWhatSection = ({ text, tally, choiceClasses, labelClasses }) =
 };
 
 export const EventWhereSection = ({ text, tally, choiceClasses, labelClasses }) => {
+
+    let placeNameClasses = classnames({
+        'long': text.placeName.length > 18
+    });
+
     return (
         <div className="poll-option-container row">
             <div className={ labelClasses }>Where</div>
             <div className={ choiceClasses }>
                 <i className="fa fa-map-marker" ariaHidden="true" />
-                { text.placeName || "TBC" }
-                <br/>
-                <span className="placeAddress">
-                    { text.placeAddress }
+                <span className={ placeNameClasses }>
+                    { text.placeName || "TBC" }
                 </span>
+                <br/>
+                {
+                    text.placeAddress !== '' &&
+                    <span className="placeAddress">
+                        { text.placeAddress }
+                    </span>
+                }
             </div>
             <span className="tally two columns">{ tally }</span>
         </div>
